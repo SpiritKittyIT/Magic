1ba0ad1f8107eceda1ff3e88a1274723adbd30b4
SpiritKitty <marian.lalik123@gmail.com>
2023-03-26 21:43:29 +0200

    first prototype + mockup spells

---
+++ MagicScript/Main.cs
@@ -0,0 +1,93 @@
+﻿using MagicScript;
+using System.Text.Json;
+
+Spell fireball = new Spell
+{
+    target = new Target { maxDistance = 150, type = TargetType.Direction },
+    effect = Effect.Discharge,
+    content = new Spell
+    {
+        target = new Target { maxDistance = 20, type = TargetType.Radius },
+        effect = Effect.Discharge,
+        condition = new Spell { element = Element.Force },
+        content = new Spell { element = Element.Heat }
+    }
+};
+
+Spell wallOfForce = new Spell
+{
+    target = new Target { maxDistance = 150, type = TargetType.Direction },
+    effect = Effect.Discharge,
+    content = new Spell
+    {
+        target = new Target { minDistance = 16, maxDistance = 20, type = TargetType.Radius },
+        effect = Effect.Discharge,
+        condition = new Spell { element = Element.Force },
+        duration = 10,
+        content = new Spell { element = Element.Force }
+    }
+};
+
+Spell light = new Spell
+{
+    target = new Target { type = TargetType.Touch },
+    effect = Effect.Discharge,
+    content = new Spell
+    {
+        target = new Target { maxDistance = 40, type = TargetType.Radius },
+        effect = Effect.Discharge,
+        duration = 60,
+        content = new Spell { element = Element.Light }
+    }
+};
+
+Spell arcaneEye = new Spell
+{
+    effect = Effect.Absorb,
+    content = new Spell
+    {
+        target = new Target { maxDistance = 30, type = TargetType.Radius },
+        condition = new Spell { element = Element.Light },
+        duration = 60
+    },
+    element = Element.Mind
+};
+
+Spell HoldPerson = new Spell
+{
+    target = new Target { maxDistance = 60, type = TargetType.Direction },
+    effect = Effect.Discharge,
+    content = new Spell
+    {
+        target = new Target { maxDistance = 1, type = TargetType.Radius },
+        effect = Effect.Absorb,
+        condition = new Spell { element = Element.Mind },
+        duration = 1,
+        element = Element.Mind
+    }
+};
+
+Spell ShapeWater = new Spell
+{
+    target = new Target { maxDistance = 5, type = TargetType.Radius },
+    effect = Effect.Shape,
+    content = new Spell { element = Element.Water },
+    duration = 60,
+};
+
+Spell Alarm = new Spell
+{
+    target = new Target { type = TargetType.Touch },
+    effect = Effect.Discharge,
+    content = new Spell
+    {
+        effect = Effect.Absorb,
+        content = new Spell
+        {
+            target = new Target { maxDistance = 30, type = TargetType.Radius },
+            condition = new Spell { element = Element.Mind }
+        }
+    }
+};
+
+Console.WriteLine(JsonSerializer.Serialize(fireball, options: new() { WriteIndented = true }));
---
+++ MagicScript/Spell.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MagicScript
+{
+    internal enum TargetType
+    {
+        Touch, Direction, Radius
+    }
+
+    internal enum Effect
+    {
+        Discharge, Absorb, Shape
+    }
+
+    internal enum Element
+    {
+        Heat, Water, Cold, Earth, Wind, Metal, Lightning, Force, Life, Death, Mind, Light
+    }
+
+    internal class Target
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? maxDistance { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? minDistance { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public TargetType type { get; set; }
+    }
+
+    internal class Spell
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Target? target { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Effect? effect { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? duration { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Spell? condition { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Spell? content { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Element? element { get; set; }
+    }
+}
